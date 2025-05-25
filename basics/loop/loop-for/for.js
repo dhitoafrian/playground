2d4c48a45b62856a74e408bc4b723a76ab8bd0e6
dhitoafrian <dhitoafrian232@gmail.com>
2025-05-25 18:44:45 +0700

    Update for.js
    bermain lebih kompleks

--- basics/loop/loop-for/for.js
+++ basics/loop/loop-for/for.js
@@ -1,9 +1,61 @@
-let tinggi = 10;
-
-for (let i = 1; i <= tinggi; i++) {
-    let bintang = ''
-    for (let b = 1; b <= i; b++) {
-        bintang += '* '
-    }
-    console.log(bintang)
+// 1. SEGITIGA KEBALIK
+let string = '';
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j <= i; j++) string += '&nbsp;';
+    for (let k = 0; k < 5 - i; k++) string += '* ';
+    string += '<br>';
 }
+document.getElementById('segitiga-kebalik').innerHTML = string;
+
+//2. JEJER GENJANG
+string = '';
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j <= i; j++) string += '&nbsp;';
+    for (let k = 0; k < 5; k++) string += '* ';
+    string += '<br>';
+}
+document.getElementById('jajar-genjang').innerHTML = string;
+
+//3. SEGITIGA NORMAL
+string = '';
+for (let i = 1; i <= 5; i++) {
+    for (let j = 5; j > i; j--) string += '&nbsp;';
+    for (let k = 0; k < i; k++) string += '* ';
+    string += '<br>';
+}
+document.getElementById('segitiga-normal').innerHTML = string;
+
+//4. BELAH KETUPAT
+string = '';
+for (let i = 0; i <= 5; i++) {
+    for (let j = 5; j > i; j--) string += '&nbsp;';
+    for (let k = 0; k < i; k++) string += '* ';
+    string += '<br>';
+}
+for (let i = 4; i > 0; i--) {
+    for (let j = 4; j >= i; j--) string += ' ';
+    for (let k = 0; k < i; k++) string += '* ';
+    string += '<br>';
+}
+document.getElementById('Belah-Ketupat').innerHTML = string;
+
+
+//5. PERSEGI
+string = '';
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+        string += (i === 0 || i === 4 || j === 0 || j === 4) ? '* ' : '  ';
+    }
+    string += '<br>';
+}
+document.getElementById('Persegi').innerHTML = string;
+
+//6. PERSEGI PANJANG
+string = '';
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 10; j++) {
+        string += (i === 0 || i === 4 || j === 0 || j === 9) ? '* ' : '&nbsp;&nbsp;';
+    }
+    string += '<br>';
+}
+document.getElementById('Persegi-Panjang').innerHTML = string;
